2c5227273182c0e82ead4f458a08687fb20374dd
El John <bongaeljohn07@gmail.com>
2026-05-03 22:18:52 +0800

    add router name on create user endpoint

--- src/users/router.py
+++ src/users/router.py
@@ -7,7 +7,12 @@ from .services import create_user
 router = APIRouter(prefix="/users", tags=["users"])
 
 
-@router.post("/", response_model=UserPublic, status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/",
+    name="Create user",
+    response_model=UserPublic,
+    status_code=status.HTTP_201_CREATED,
+)
 async def create_user_handler(user: UserCreate, db: DatabaseDep):
     new_user = await create_user(db, user)
     return new_user
