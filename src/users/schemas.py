0c2981e7d186a838ef858b819373b62eca3c1cee
El John <bongaeljohn07@gmail.com>
2026-05-07 12:14:01 +0800

    add model config to user schemas

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -1,12 +1,22 @@
 from pydantic import BaseModel, ConfigDict, EmailStr, Field
+from pydantic.alias_generators import to_camel
 
 
-class Token(BaseModel):
+class BaseSchemaModel(BaseModel):
+    model_config = ConfigDict(
+        from_attributes=True,
+        validate_by_name=True,
+        serialize_by_alias=True,
+        alias_generator=to_camel,
+    )
+
+
+class Token(BaseSchemaModel):
     access_token: str
     token_type: str
 
 
-class UserBase(BaseModel):
+class UserBase(BaseSchemaModel):
     first_name: str = Field(min_length=1, max_length=50)
     last_name: str = Field(min_length=1, max_length=50)
     email: EmailStr = Field(max_length=120)
@@ -16,9 +26,7 @@ class UserCreate(UserBase):
     password: str = Field(min_length=8)
 
 
-class UserPublic(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
-
+class UserPublic(BaseSchemaModel):
     id: int
     profile_image: str | None
     first_name: str
@@ -30,7 +38,7 @@ class UserPrivate(UserPublic):
     email: EmailStr
 
 
-class UserUpdate(BaseModel):
+class UserUpdate(BaseSchemaModel):
     first_name: str | None = Field(default=None, min_length=1, max_length=50)
     last_name: str | None = Field(default=None, min_length=1, max_length=50)
     email: EmailStr | None = Field(default=None, max_length=120)
